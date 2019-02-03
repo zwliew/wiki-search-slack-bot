e45708c43686930f8258cc41e3623383351cbafa
Zhao Wei Liew <zhaoweiliew@gmail.com>
2019-02-03 18:57:39 +0800

    Install helmet middleware

--- server/index.mjs
+++ server/index.mjs
@@ -1,10 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
+import helmet from "helmet";
 
 import router from "./router";
 
 const app = express();
 
+app.use(helmet());
 app.use(bodyParser.json());
 app.use("/api/v1", router);
 

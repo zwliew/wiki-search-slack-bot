ff7f5bd4a7d3bc7d7af633bed3e64af9616b7dbe
Zhao Wei Liew <zhaoweiliew@gmail.com>
2019-02-03 17:51:59 +0800

    Get rid of the initialized flag

---
+++ server/browser.mjs
@@ -0,0 +1,46 @@
+import puppeteer from "puppeteer";
+
+// Starts a new Google-authenticated browser session and
+// returns the current page for browsing
+async function browse(url) {
+  // Disable the sandbox to run on Heroku
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto(url);
+
+  // Log into Google account
+  await page.waitForSelector('input[type="email"]');
+  await page.type('input[type="email"]', process.env.GOOG_USR);
+  await page.click("#next");
+  await page.waitForSelector('input[type="password"]', { visible: true });
+  await page.type('input[type="password"]', process.env.GOOG_PWD);
+
+  await Promise.all([
+    page.click('input[type="submit"]'),
+    page.waitForNavigation()
+  ]);
+
+  // The two-factor verification process only appears during
+  // a 'suspicious' sign-in attempt
+  try {
+    await Promise.all([
+      page.click('input[type="submit"]'),
+      page.waitForNavigation()
+    ]);
+
+    const code = await (await page.$('div[jsname="EKvSSd"]')).getProperty(
+      "textContent"
+      );
+    console.log(`Code: ${code}`);
+  } catch (err) {
+    // Do nothing
+  }
+
+  return page;
+}
+
+export default browse;
--- server/search.mjs
+++ server/search.mjs
@@ -1,53 +1,10 @@
-import puppeteer from "puppeteer";
+import browse from './browser';
 
-// Provide the Google username and password as environment variables
-const GOOG_USR = process.env.GOOG_USR;
-const GOOG_PWD = process.env.GOOG_PWD;
 const GOOG_SITES_URL =
   "https://sites.google.com/search/tinkertanker.com/tinkertanker-wiki?query=";
 
+// Stores the current page (should be Google-authenticated)
 let page = null;
-let initialized = false;
-async function init() {
-  // Disable the sandbox to run on Heroku
-  const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
-  });
-
-  page = await browser.newPage();
-
-  await page.goto(GOOG_SITES_URL);
-
-  // Log into Google account
-  await page.waitForSelector('input[type="email"]');
-  await page.type('input[type="email"]', GOOG_USR);
-  await page.click("#next");
-  await page.waitForSelector('input[type="password"]', { visible: true });
-  await page.type('input[type="password"]', GOOG_PWD);
-
-  await Promise.all([
-    page.click('input[type="submit"]'),
-    page.waitForNavigation()
-  ]);
-
-  // The two-factor verification process only appears during
-  // a 'suspicious' sign-in attempt
-  try {
-    await Promise.all([
-      page.click('input[type="submit"]'),
-      page.waitForNavigation()
-    ]);
-
-    const code = await (await page.$('div[jsname="EKvSSd"]')).getProperty(
-      "textContent"
-    );
-    console.log(`Code: ${code}`);
-  } catch (err) {
-    // Do nothing
-  }
-
-  initialized = true;
-}
 
 // Search the wiki with a given query string
 async function search(query) {
@@ -69,11 +26,8 @@ async function search(query) {
 // Scrape the given URL for the relevant data and format it
 async function scrape(url) {
   // Initialize the browser session if not already done
-  if (!initialized) {
-    // The browser is initializing, but not yet done
-    if (page) return [];
-
-    init();
+  if (!page) {
+    browse(url).then(ret => page = ret);
 
     // Skip handling this request to allow time to sign into Google
     console.log("Skipping request");
